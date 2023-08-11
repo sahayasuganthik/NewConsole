a1e2993303e1beba24d5a0ceda787c05d0980f6f
SahayaSuganthi <dsahayam1987@gmail.com>
2023-08-11 14:10:00 +0530

    Updated new

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int test = 1234;
+
+var digits = test.ToString().Select(x => int.Parse(x.ToString())); ;
+
+Console.WriteLine(digits.Last());
---
+++ MyConsoleApp/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Sample 
+    {
+        public void GetTotalForPrices()
+        {
+            int test = 1234;
+
+            var digits = test.ToString().Select(x => int.Parse(x.ToString())); ;
+
+            Console.WriteLine(digits);
+        }
+    }
+}
